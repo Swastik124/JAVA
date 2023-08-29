c7b601eaca40192bd1f43d71284b6c8cffbe2d28
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-08-29 22:44:46 +0530

    Add files via upload

---
+++ TakingInput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TakingInput{
+    public static void main(String[] args)
+    {
+        System.out.println("Input any Number=");
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println("The Entered Number is: "+x);
+    }
+}
