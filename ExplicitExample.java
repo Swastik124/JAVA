e462a25960b2edb65ad670ce5e50ab3d3034dcaa
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-09-02 20:10:03 +0530

    Add files via upload

---
+++ ExplicitExample.java
@@ -0,0 +1,10 @@
+public class ExplicitExample {
+    
+    public static void main(String[] args)
+    {
+        byte y;
+        int x=130;
+        y=(byte)x;   //Explicit Example
+        System.out.println("y="+y);
+    }
+}
