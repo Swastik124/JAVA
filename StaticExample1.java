01e0df6aa901bc2c8e69f183d152e150da1bc174
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-09-11 21:27:14 +0530

    Add files via upload

---
+++ StaticExample1.java
@@ -0,0 +1,25 @@
+class Student {
+        int regdno;
+        String name;
+        static String cname="Silicon";
+        Student(int regdno,String name)
+        {
+            this.regdno=regdno;
+            this.name=name;
+        }
+        void show()
+        {
+            System.out.println(regdno+" "+name);
+            System.out.println(cname);
+        }
+}
+class StaticExample1
+{
+    public static void main(String[] args)
+    {
+        Student s1= new Student(101,"John");
+        Student s2= new Student(102,"Mike");
+        s1.show();
+        s2.show();
+    }
+}
