90315a8a8b7e0e3b2eba1055634d4cfb94e6fc93
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-09-21 19:37:58 +0530

    Add files via upload

---
+++ ArrayPairs.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ArrayPairs {
+
+    public static void input(int a[],int s)
+    {
+        System.out.println("Enter the Array values= ");
+        Scanner p= new Scanner(System.in);
+        for(int i=0;i<s;i++)
+        {
+            a[i]= p.nextInt();
+        }
+    }
+    public static void display(int a[],int s)
+    {
+        int j,count=0;
+        while(count!=s-1)
+        {
+            for(j=count;j<s-1;j++)
+            {
+                if(a[j]!=a[s-1])
+                    System.out.println(a[j]+" "+a[j+1]);
+            }
+            ++count;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int s;
+        System.out.println("Enter the size of Array= ");
+        Scanner sc= new Scanner(System.in);
+        s=sc.nextInt();
+        int a[]= new int[s];
+        input(a,s);
+        System.out.println("Array Pairs are: ");
+        display(a,s);
+    }
+}
