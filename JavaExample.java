b72b3d811c69e40debc583d84b6128165ab1870d
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-10-31 22:53:04 +0530

    Add files via upload

---
+++ JavaExample.java
@@ -0,0 +1,13 @@
+class JavaExample {
+    static int num;
+    static String str;
+    static{
+        num=97;
+        str="Static Keyword in Java";
+    }
+    public static void main (String[] args)
+    {
+        System.out.println("value of num= "+num);
+        System.out.println("value of str= "+str);
+    }
+}
