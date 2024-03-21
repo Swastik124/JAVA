3ca6070e89eaa6accb2fcc318fa40729b2a59233
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-03-21 21:28:59 +0530

    Update BitwiseExample.java

--- BitwiseExample.java
+++ BitwiseExample.java
@@ -1,6 +1,7 @@
 public class BitwiseExample {
     public static void main(String[] args)
     {
+        
         int x=5,y=3;
         int result1,result2,result3,result4;
         result1=x&y;
@@ -11,5 +12,6 @@ public class BitwiseExample {
         System.out.println("result="+result3);
         result4=~x;
         System.out.println("result="+result4);
+        
     }
 }
