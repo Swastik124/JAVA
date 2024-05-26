00a6504612eab5921670a6392f7d5f81bb354dbe
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-05-26 22:45:59 +0530

    Update JavaExample.java

--- JavaExample.java
+++ JavaExample.java
@@ -1,13 +1,18 @@
 class JavaExample {
+    
     static int num;
     static String str;
     static{
+        
         num=97;
         str="Static Keyword in Java";
+    
     }
     public static void main (String[] args)
     {
+        
         System.out.println("value of num= "+num);
         System.out.println("value of str= "+str);
+    
     }
 }
