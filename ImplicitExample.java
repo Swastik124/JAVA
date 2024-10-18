b7b8ae8eecf8e7984d3371fcbc258e818d3f6253
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-10-18 22:51:54 +0530

    Update ImplicitExample.java

--- ImplicitExample.java
+++ ImplicitExample.java
@@ -2,9 +2,11 @@ class ImplicitExample
 {
     public static void main(String[] args)
     {
+        
         byte y=30;
         int x;
         x=y;
         System.out.println("x="+x);
     }
+    
 }
